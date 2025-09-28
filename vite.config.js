9150db74c9ed857349345f46d67a48926ff299cf
Tesneem Uyghur <tawuti@my.centennialcollege.ca>
2025-09-28 11:54:11 -0400

    Fix vite config to include index.html

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    manifest: true,
-    rollupOptions: {
-      input: "./src/main.jsx",
-    },
-  },
+    outDir: 'dist'
+  }
 });
